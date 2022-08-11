ee42f11008d2c9f377ae5755f2d7ead6b9128a4e
AbakumovaOlga <31732995+AbakumovaOlga@users.noreply.github.com>
2022-08-11 12:23:39 +0300

    not work

---
+++ src/main/java/com/example/tictactoe/config/WebSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.example.tictactoe.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public UserDetailsManager users(DataSource dataSource) {
+        UserDetails user = User.withDefaultPasswordEncoder()
+            .username("user")
+            .password("password")
+            .roles("USER")
+            .build();
+        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+        users.createUser(user);
+        return users;
+    }
+}
